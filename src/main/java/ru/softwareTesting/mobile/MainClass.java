ec1449551c81270cac1059eb6fc762feaafb064c
Григорчук Кирилл <kgrigorchuk@it-one.ru>
2023-06-16 15:40:36 +0300

    added new implementation (lombok + slf4j)

--- src/main/java/ru/softwareTesting/mobile/MainClass.java
+++ src/main/java/ru/softwareTesting/mobile/MainClass.java
@@ -1,10 +1,20 @@
 package ru.softwareTesting.mobile;
 
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class MainClass {
     public static final int MAGIC_NUMBER = 14;
+    private int class_number = 20;
+
 
     public int getLocalNumber(){
         return MAGIC_NUMBER;
+    }
+
+    public int getClassNumber(){
+        return class_number;
     }
 
 }
--- src/test/java/MainClassTest.java
+++ src/test/java/MainClassTest.java
@@ -3,19 +3,30 @@ import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import ru.softwareTesting.mobile.MainClass;
 
+
 public class MainClassTest {
 
+    MainClass Main = new MainClass();
     @Test
     public void testGetLocalNumber() {
-        MainClass main = new MainClass();
+
         int expectedNumber = 14;
-        int actualNumber = main.getLocalNumber();
+        int actualNumber = Main.getLocalNumber();
 
         MatcherAssert.assertThat(
                 String.format("Error in comparing two values. Expected value = %s , actual value = %s",
                         expectedNumber, actualNumber),
                 actualNumber,
                 Matchers.is(expectedNumber));
+    }
+
+    @Test
+    public void testGetClassNumber(){
+
+        if (Main.getClassNumber() > 45){
+        }
+
+
     }
 
 
