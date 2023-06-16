1299af449aac70bb74af6ffc514222cc7d25b1b4
Григорчук Кирилл <kgrigorchuk@it-one.ru>
2023-06-16 16:38:28 +0300

    * Test 2: В классе MainClassTest написать тест (назвать testGetClassNumber), который проверяет, что метод getClassNumber возвращает число больше 45.

--- src/main/java/ru/softwareTesting/mobile/MainClass.java
+++ src/main/java/ru/softwareTesting/mobile/MainClass.java
@@ -3,7 +3,7 @@ package ru.softwareTesting.mobile;
 public class MainClass {
     public static final int MAGIC_NUMBER = 14;
     private int class_number = 20;
-
+    private String class_string = "Hello, world";
 
     public int getLocalNumber(){
         return MAGIC_NUMBER;
@@ -13,4 +13,5 @@ public class MainClass {
         return class_number;
     }
 
+    public String getClassString(){ return class_string;}
 }
--- src/test/java/MainClassTest.java
+++ src/test/java/MainClassTest.java
@@ -27,10 +27,16 @@ public class MainClassTest {
         int actualNumber = Main.getClassNumber();
         if (actualNumber > 45){
             log.info("testGetClassNumber: The actual number = {} is greater than 45.", actualNumber);
+            MatcherAssert.assertThat("The actual number is greater than 45.", true);
         } else {
             log.error("testGetClassNumber: The actual number = {} is less than 45.", actualNumber);
+            MatcherAssert.assertThat("The actual number is less than 45.", false);
         }
     }
 
+    @Test
+    public void testGetClassString(){
+        String actualText = Main.getClassString();
+    }
 
 }
